2c0707e8c6f1ddb2b939d55294c368da9c234886
Milan Fusco <milanfusco68@gmail.com>
2026-01-14 23:00:02 -0500

    Add requirements file and initialize pi_estimation module

---
+++ pi_estimation/__init__.py
@@ -0,0 +1,10 @@
+"""
+McFramework-GPU: GPU-Accelerated Monte Carlo Computing
+
+A comparative study of CPU (Numpy) vs GPU (PyTorch) tensor 
+operations for Monte Carlo estimation of Pi.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Milan Fusco"
+
